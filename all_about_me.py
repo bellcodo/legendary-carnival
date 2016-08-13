2c9d93d365b1efddeaf0ab083cdc5d9914a92c7b
Bell Codo <bellcodo@gmail.com>
2016-08-12 17:49:02 -0700

    Create all_about_me.py

---
+++ all_about_me.py
@@ -0,0 +1,10 @@
+print """
+I came to teach young people 
+about programming and 
+give them a place to learn together.
+
+I hope to learn more about leading the group
+I also want to know more about what the new 
+wave/generation is capable of and
+learn new programming tips from the volunteers.
+"""
